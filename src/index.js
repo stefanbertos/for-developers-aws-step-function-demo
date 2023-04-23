8956056df40c1ee0c8ac732e00f2dafb332f58f2
Stefan <stefan.bertos@gmail.com>
2023-04-23 18:23:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from "@aws-amplify/ui-react";
-import { Amplify } from 'aws-amplify';
+import {Amplify, AuthModeStrategyType} from 'aws-amplify';
 
 import awsconfig from './aws-exports';
 
 import "@aws-amplify/ui-react/styles.css";
 import { studioTheme } from "./ui-components";
 
-Amplify.configure(awsconfig);
+Amplify.configure({
+    ...awsconfig,
+    DataStore: {
+        // The default configuration for observeQuery has a default syncPageSize of 1,000 and a default maxRecordsToSync of 10,000. These values can be customized by configuring their values manually in DataStore.configure.
+        maxRecordsToSync: 100000,
+        syncPageSize: 10000,
+        fullSyncInterval: 24*60, //24 hodin
+        authModeStrategyType: AuthModeStrategyType.MULTI_AUTH,
+        errorHandler: (error) => {
+            console.log("DataStore unrecoverable error", JSON.stringify(error));
+        }
+    }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
